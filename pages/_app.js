63d14120bb7e12ac64848da3023f4f656443de46
mohammad-abeer-geekybugs <m.abeer@geekybugs.com>
2023-02-06 16:41:03 +0500

    resolved _app and _document issues

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Navbar from "@/components/Navbar";
 import createEmotionCache from "../utils/createEmotionCache";
 import theme from "../styles/theme/theme";
 import "../styles/globals.css";
-// import Head from "next/head";
+import Head from "next/head";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -15,25 +15,21 @@ const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        {/* <Head>
-          <title>Smart Home</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap"
-            rel="stylesheet"
-          />
-        </Head> */}
-        {/* ----- NavBar ----- */}
-        <Navbar />
-        {/* ----- Application ----- */}
-        <Component {...pageProps} />
-        {/* ----- Footer ----- */}
-        <Footer />
-      </ThemeProvider>
-    </CacheProvider>
+    <>
+      <Head>
+        <title>House Smarts</title>
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          {/* ----- NavBar ----- */}
+          <Navbar />
+          {/* ----- Application ----- */}
+          <Component {...pageProps} />
+          {/* ----- Footer ----- */}
+          <Footer />
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   );
 };
 
